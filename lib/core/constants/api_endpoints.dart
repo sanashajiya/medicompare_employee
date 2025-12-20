419dcf045a14a666ba7cc607befb25e836af7f71
sana shaik <ssanashajiya@gmail.com>
2025-12-20 15:52:34 +0530

    change app icon

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,6 +1,6 @@
 class ApiEndpoints {
-  // static const String baseUrl = 'https://api.medicompares.digitalraiz.co.in/api/v1';
-  static const String baseUrl = 'http://192.168.0.161:9001/api/v1';
+  static const String baseUrl = 'https://api.medicompares.digitalraiz.co.in/api/v1';
+  // static const String baseUrl = 'http://192.168.0.161:9001/api/v1';
 
   // Authentication endpointss
   static const String login = '$baseUrl/employeevendor/auth/login';
@@ -14,6 +14,7 @@ class ApiEndpoints {
 
   // Vendor endpoints
   static const String createVendor = '$baseUrl/employeevendor/vendor/create';
+  static const String updateVendor = '$baseUrl/employeevendor/vendor/update';
   static const String sendVendorProfileOtp =
       '$baseUrl/employeevendor/vendor/profile/send-otp';
   // static const String verifyVendorProfileOtp =
@@ -21,6 +22,7 @@ class ApiEndpoints {
   static const String getCategories = '$baseUrl/common/medicalcategories';
   static const String getDashboard = '$baseUrl/employeevendor/dashboard';
   static const String getVendorList = '$baseUrl/employeevendor/vendor/list/all';
+  static const String getVendorDetails = '$baseUrl/employeevendor/vendor/details';
 
   ApiEndpoints._();
 }
